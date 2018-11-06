11ccdda1d4eea2209a60e2db54f61d19c6d34526
kedaroo <krb1202@gmail.com>
2018-11-06 22:47:35 +0530

    .

--- Tests.py
+++ Tests.py
@@ -25,3 +25,20 @@ print(b.get_balance('Kevin'))
 
 # list of pending transactions
 print(f'Pending transactions: {b.pendingTransactions}')
+
+# OUTPUT
+
+# Blockchain:
+#
+# Timestamp: Tue Nov  6 22:43:23 2018
+# Transactions: []
+# Previous Hash: 0
+# Hash: cc8eeacd5d2df0d43208792a0d74886ee6e609b2baf03a0c28d7f60c9d18aac3
+# Nonce: 0
+# Pending transactions: [From: John, To: Jane, Amount: 600, From: John, To: Megan, Amount: 700, From: John, To: Chris, Amount: 800]
+# JOHN is worth -2100 cryptocoins
+# JANE is worth 600 cryptocoins
+# MEGAN is worth 700 cryptocoins
+# CHRIS is worth 800 cryptocoins
+# KEVIN is worth 100 cryptocoins
+# Pending transactions: []
